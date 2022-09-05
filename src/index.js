4a9b76199a592dbac822584306e20e7ac505e91c
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-05 12:38:09 +0530

    feat: added context provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CartProvider } from './context/cartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <CartProvider>
     <App />
-  </React.StrictMode>
+  </CartProvider>
 );
