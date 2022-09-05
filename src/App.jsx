7f8075a90dfb2af8fb2a496b8e61fb60c4a5a31b
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-05 12:37:18 +0530

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import styled from 'styled-components';
+import Cart from './pages/Cart';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './globalStyles';
+import Wishlist from './pages/Wishlist';
+
+const Container = styled.div`
+  overflow: hidden;
+`;
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Container>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/productList' element={<ProductList />} />
+          <Route exact path='/product' element={<Product />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/cart' element={<Cart />} />
+          <Route exact path='/wishlist' element={<Wishlist />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 };
 
