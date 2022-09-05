9b6e235955a30e6bf82894dc7ecf929d2e0119d3
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-05 12:37:45 +0530

    style: global css for the spp

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+a {
+  text-decoration: none;
+  color: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+`;
