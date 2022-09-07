7bd14f5a52f2bb97fc49d0b20f7e6ebe722f826a
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-07 22:53:16 +0530

    wip

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import WishListItem from '../components/WishListItem';
+import { useCart } from '../context/cartContext';
 import { mobile } from '../responsive';
 
 const Container = styled.div``;
@@ -49,7 +51,6 @@ const Bottom = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 1rem;
 
   ${mobile({
     // width: '75%',
@@ -60,119 +61,35 @@ const Bottom = styled.div`
   })}
 `;
 
-const Product = styled.div`
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  ${mobile({ flexDirection: 'column' })}
-`;
-
-const ProductDetail = styled.div`
-  display: flex;
-`;
-
-const Image = styled.img`
-  width: 200px;
-`;
-
-const Details = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-const ProductName = styled.div`span`;
-
-const ProductId = styled.div`span`;
-
-const ProductColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${props => props.color};
-`;
-
-const ProductSize = styled.div`span`;
-
-const Button = styled(TopButton)`
-  height: min-content;
-  display: flex;
-  align-self: center;
-  justify-self: center;
-`;
-
-const Hr = styled.hr`
-  height: 0.5px;
-  width: 50%;
-  background-color: #e4e4e4;
-  outline: none;
-  border: none;
-`;
-
 const Wishlist = () => {
-  const navigate = useNavigate();
+  const {
+    state: { cart, wishlist },
+    dispatch,
+  } = useCart();
 
-  const handleGoBackToShopping = () => {
-    navigate('/productList');
-  };
+  const navigate = useNavigate();
 
   return (
     <Container>
       <Navbar />
       <Announcement />
       <Wrapper>
-        <Title>YOUR WISHLIST</Title>
+        <Title>YOUR WISHLIST ({wishlist.length})</Title>
         <Top>
-          <TopButton onClick={handleGoBackToShopping}>
+          <TopButton onClick={() => navigate('/productList')}>
             CONTINUE SHOPPING
           </TopButton>
           <TopTexts>
-            <TopText>Shopping Bag (2)</TopText>
-            <TopText>Your Wishlist (0)</TopText>
+            <TopText onClick={() => navigate('/cart')}>
+              Shopping Bag ({cart.length})
+            </TopText>
           </TopTexts>
-          <TopButton type='filled'>CHECKOUT NOW</TopButton>
         </Top>
+
         <Bottom>
-          <Product>
-            <ProductDetail>
-              <Image src='https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png' />
-              <Details>
-                <ProductName>
-                  <b>Product:</b> Jimmny Shoes
-                </ProductName>
-                <ProductId>
-                  <b>ID:</b> 2423434243224324
-                </ProductId>
-                <ProductColor color='black' />
-                <ProductSize>
-                  <b>Size:</b> 37.5
-                </ProductSize>
-              </Details>
-            </ProductDetail>
-            <Button type='filled'>ADD TO CART</Button>
-          </Product>
-          <Hr />
-          <Product>
-            <ProductDetail>
-              <Image src='https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png' />
-              <Details>
-                <ProductName>
-                  <b>Product:</b> Jimmny Shoes
-                </ProductName>
-                <ProductId>
-                  <b>ID:</b> 2423434243224324
-                </ProductId>
-                <ProductColor color='black' />
-                <ProductSize>
-                  <b>Size:</b> 37.5
-                </ProductSize>
-              </Details>
-            </ProductDetail>
-            <Button type='filled'>ADD TO CART</Button>
-          </Product>
-          <Hr />
+          {wishlist.map(item => (
+            <WishListItem dispatch={dispatch} item={item} key={item.id} />
+          ))}
         </Bottom>
       </Wrapper>
       <Footer />
