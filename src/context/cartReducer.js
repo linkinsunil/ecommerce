1760a58d4a9e5b411c04868d122c603873fa880b
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-07 22:49:45 +0530

    added cart and wishlist reducer

--- src/context/cartReducer.js
+++ src/context/cartReducer.js
@@ -1,21 +1,33 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    case 'ADD':
+    case 'ADD_TO_CART':
       return {
-        // ...state,
-        // cart: [...state.cart, state.cart.push(action.payload)],
-        // totalItems: state.totalItems + action.payload,
-        // totalPrice: state.totalPrice + action.payload,
+        ...state,
+        cart: [...state.cart, { ...action.payload, qty: 1 }],
       };
-    case 'REMOVE':
+    case 'REMOVE_FROM_CART':
       return {
-        // ...state,
-        // cart: [
-        //   ...state.cart,
-        //   state.cart.filter(item => !item === action.payload),
-        // ],
-        // totalItems: state.totalItems + action.payload,
-        // totalPrice: state.totalPrice + action.payload,
+        ...state,
+        cart: state.cart.filter(item => item.id !== action.payload.id),
+      };
+    case 'CHANGE_CART_QTY':
+      return {
+        ...state,
+        cart: state.cart.filter(item =>
+          item.id === action.payload.id
+            ? (item.qty = action.payload.qty)
+            : item.qty
+        ),
+      };
+    case 'ADD_TO_WISHLIST':
+      return {
+        ...state,
+        wishlist: [...state.wishlist, { ...action.payload }],
+      };
+    case 'REMOVE_FROM_WISHLIST':
+      return {
+        ...state,
+        wishlist: state.wishlist.filter(item => item.id !== action.payload.id),
       };
     default:
       return state;
