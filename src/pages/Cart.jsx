99ca703a7b7d499d9193aeda74ee7eb544e50b8a
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-09-07 22:50:19 +0530

    added navigation and data

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
-import { Add, Remove } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
+import CartItem from '../components/CartItem';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import { useCart } from '../context/cartContext';
 import { mobile } from '../responsive';
 
 const Container = styled.div``;
@@ -48,7 +50,7 @@ const TopText = styled.span`
 
 const Bottom = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
 
   ${mobile({
     // width: '75%',
@@ -59,186 +61,45 @@ const Bottom = styled.div`
   })}
 `;
 
-const Info = styled.div`
-  flex: 3;
-`;
-
-const Product = styled.div`
-  display: flex;
-  justify-content: space-between;
-
-  ${mobile({ flexDirection: 'column' })}
-`;
-
-const ProductDetail = styled.div`
-  flex: 2;
-  display: flex;
-`;
-
-const Image = styled.img`
-  width: 200px;
-`;
-
-const Details = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-const ProductName = styled.div`span`;
-
-const ProductId = styled.div`span`;
-
-const ProductColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${props => props.color};
-`;
-
-const ProductSize = styled.div`span`;
-
-const PriceDetail = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  ${mobile({ marginBottom: '10px' })}
-`;
-
-const ProductAmountContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-`;
-const ProductAmount = styled.div`
-  font-size: 24px;
-  margin: 5px;
-
-  ${mobile({ margin: '0 15px' })}
-`;
-const ProductPrice = styled.span`
-  font-size: 30px;
-  font-weight: 200;
-`;
-
-const Hr = styled.div`
-  background-color: #eee;
-  border: none;
-  height: 1px;
-
-  ${mobile({ display: 'none' })}
-`;
-
-const Summary = styled.div`
-  flex: 1;
-  border: 0.5px solid lightgray;
-  border-radius: 10px;
-  padding: 20px;
-  height: max-content;
-
-  ${mobile({ width: '100%' })}
-`;
-
 const SummaryTitle = styled.h1`
   font-weight: 200;
 `;
 
-const SummaryItem = styled.div`
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-  font-weight: ${props => props.type === 'total' && '500'};
-  font-size: ${props => props.type === 'total' && '24px'};
-`;
-
-const SummaryItemText = styled.span``;
-
-const SummaryItemPrice = styled.span``;
-
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: black;
-  color: white;
-  font-weight: 600;
-`;
-
 const Cart = () => {
-  const navigate = useNavigate();
+  const {
+    state: { cart, wishlist },
+    dispatch,
+  } = useCart();
 
-  const handleGoBackToShopping = () => {
-    navigate('/productList');
-  };
+  const navigate = useNavigate();
 
   return (
     <Container>
       <Navbar />
       <Announcement />
       <Wrapper>
-        <Title>YOUR BAG</Title>
+        <Title>YOUR BAG ({cart.length})</Title>
         <Top>
-          <TopButton onClick={handleGoBackToShopping}>
+          <TopButton onClick={() => navigate('/productList')}>
             CONTINUE SHOPPING
           </TopButton>
           <TopTexts>
-            <TopText>Shopping Bag (2)</TopText>
-            <TopText>Your Wishlist (0)</TopText>
+            <TopText onClick={() => navigate('/wishlist')}>
+              Your Wishlist ({wishlist.length})
+            </TopText>
           </TopTexts>
-          <TopButton type='filled'>CHECKOUT NOW</TopButton>
         </Top>
-        <Bottom>
-          <Info>
-            <Product>
-              <ProductDetail>
-                <Image src='https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png' />
-                <Details>
-                  <ProductName>
-                    <b>Product:</b> Jimmny Shoes
-                  </ProductName>
-                  <ProductId>
-                    <b>ID:</b> 2423434243224324
-                  </ProductId>
-                  <ProductColor color='black' />
-                  <ProductSize>
-                    <b>Size:</b> 37.5
-                  </ProductSize>
-                </Details>
-              </ProductDetail>
-              <PriceDetail>
-                <ProductAmountContainer>
-                  <Add />
-                  <ProductAmount>2</ProductAmount>
-                  <Remove />
-                </ProductAmountContainer>
-                <ProductPrice>$ 30</ProductPrice>
-              </PriceDetail>
-            </Product>
-            <Hr />
-          </Info>
-          <Summary>
-            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
-            <SummaryItem>
-              <SummaryItemText>Subtotal</SummaryItemText>
-              <SummaryItemPrice>$ 60</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Estimated Shipping</SummaryItemText>
-              <SummaryItemPrice>$ 6</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem>
-              <SummaryItemText>Shipping Discount</SummaryItemText>
-              <SummaryItemPrice>$ 6</SummaryItemPrice>
-            </SummaryItem>
-            <SummaryItem type='total'>
-              <SummaryItemText>Total</SummaryItemText>
-              <SummaryItemPrice>$ 60</SummaryItemPrice>
-            </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
-          </Summary>
-        </Bottom>
+        {cart.length > 0 ? (
+          <Bottom>
+            <CartItem cart={cart} dispatch={dispatch} />
+          </Bottom>
+        ) : (
+          <SummaryTitle
+            style={{ height: '60vh', display: 'grid', placeItems: 'center' }}
+          >
+            Cart is looking empty 🛒
+          </SummaryTitle>
+        )}
       </Wrapper>
       <Footer />
     </Container>
